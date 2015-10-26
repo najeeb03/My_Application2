8764640b243d48853e5791dd9dc25ffee69a2b3d
najeeb03 <najeeb_03@hotmail.com>
2015-10-26 20:33:19 +0600

    Expense Manager

---
+++ app/src/main/java/com/example/administrator/myapplication/LaunchActivity.java
@@ -0,0 +1,56 @@
+package com.example.administrator.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        SharedPreferences sp = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+        boolean isSet = sp.getBoolean("budget_isSet", false);
+        Intent intent;
+
+
+        if(isSet == false){
+            intent = new Intent(LaunchActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else if(isSet == true){
+            intent = new Intent(LaunchActivity.this, Main2Activity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_launch, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/myapplication/Main2Activity.java
@@ -0,0 +1,90 @@
+package com.example.administrator.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        final SharedPreferences sp = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor  = sp.edit();
+        final int balance = sp.getInt("Budget", 0);
+        String balanceStr = Integer.toString(balance);
+
+        final TextView output = (TextView) findViewById(R.id.output);
+        final EditText inputExp = (EditText) findViewById(R.id.inputExp);
+        final Button enterB = (Button) findViewById(R.id.enter_button);
+        final Button setBudget = (Button) findViewById(R.id.set_button);
+
+        output.setText(balanceStr);
+
+        enterB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int expenseInt;
+                String expense = inputExp.getText().toString();
+                inputExp.setText("");
+
+                try {
+                    expenseInt = Integer.parseInt(expense);
+                    int bal = sp.getInt("Budget", 0);
+                    editor.putInt("Budget", bal - expenseInt);
+                    editor.commit();
+                    output.setText(Integer.toString(bal));
+                } catch (Exception e) {
+                    inputExp.setHint("Please enter an integer");
+                }
+
+
+                // Log.d("Decrease", "bal " + bal + " expenseInt " + expenseInt);
+            }
+        });
+
+        setBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
